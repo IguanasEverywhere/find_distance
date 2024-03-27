0f4f5e6db61f3cd87face7fb00206cb749c1193b
Scott Schwab <scottschwab86@gmail.com>
2024-03-27 13:33:37 -0500

    Complete challenge

--- javascript/find_distance.js
+++ javascript/find_distance.js
@@ -1,10 +1,44 @@
 function findDistance(graph, vertexA, vertexB) {
-  // type your code here
+  let queue = graph[vertexA];
+  let count = 1;
+
+  let visited = [vertexA];
+
+  while (queue.length > 0) {
+    if (queue.includes(vertexB)) {
+      return count;
+    } else {
+
+      count++;
+
+      let nextItems = [];
+      let prevLength = queue.length;
+
+      for (let item of queue) {
+        for (let v of graph[item]) {
+          if (v === vertexB) {
+            return count;
+          }
+          if (!visited.includes(v)) {
+            nextItems.push(v);
+            visited.push(v);
+          }
+        }
+      }
+
+      queue = queue.concat(nextItems);
+
+      for (let i = 0; i < prevLength; i++) {
+        queue.shift();
+      }
+    }
+  }
+  return -1;
 }
 
 if (require.main === module) {
   // add your own tests in here
-  const graph = {
+  const graphSmall = {
     jan: ["john", "jambaby"],
     john: ["carl"],
     jambaby: [],
@@ -12,13 +46,21 @@ if (require.main === module) {
     dave: []
   };
 
-  console.log("Expecting: 2");
-  console.log(findDistance(graph, "jan", "carl"));
+  const graphMed = {
+    jan: ["cranberry", "jamboree"],
+    john: ["jambaby"],
+    jambaby: ["jan", "cranberry"],
+    carl: [],
+    dave: ["john", "carl"],
+    cranberry: [],
+    hamtaro: ["jambaby", "dave"],
+    jamboree: ["carl", "john"]
+  };
+
+  console.log("Expecting: 4");
+  console.log(findDistance(graphMed, "jan", "jan"));
 
   console.log("");
-
-  console.log("Expecting: -1");
-  console.log(findDistance(graph, "dave", "carl"));
 }
 
 module.exports = findDistance;
--- javascript/tests/find_distance.test.js
+++ javascript/tests/find_distance.test.js
@@ -8,7 +8,7 @@ describe("findDistance", () => {
     carl: ["jambaby"],
     dave: []
   };
-  
+
   const graphMed = {
     jan: ["cranberry", "jamboree"],
     john: ["jambaby"],
@@ -23,14 +23,14 @@ describe("findDistance", () => {
   it("returns the shortest distance between two vertices when there's a path between them", () => {
     expect(findDistance(graphSmall, "jan", "carl")).toEqual(2);
     expect(findDistance(graphMed, "dave", "carl")).toEqual(1);
-  }); 
-  
+  });
+
   it("returns -1 when there is no path between vertices", () => {
     expect(findDistance(graphSmall, "dave", "carl")).toEqual(-1);
     expect(findDistance(graphSmall, "jambaby", "carl")).toEqual(-1);
     expect(findDistance(graphMed, "jamboree", "hamtaro")).toEqual(-1);
   });
-  
+
   it("returns the correct distance when detecting cycles", () => {
     expect(findDistance(graphSmall, "jan", "jan")).toEqual(-1);
     expect(findDistance(graphMed, "jan", "jan")).toEqual(4);
